f83aa2b0be3afd04c48a31b76d71aff0eb076986
Alexandre MEUR <alex.meur@gmail.com>
2019-07-04 11:07:10 +0200

    1_Routage
    Created Header, SideMenu & Options components as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,43 @@ import {APP_CONFIG} from './app.config';
 import {TokenInterceptorService} from './services/interceptors/token-interceptor.service';
 import {ErrorInterceptorService} from './services/interceptors/error-interceptor.service';
 import {CommonHeadersInterceptorService} from './services/interceptors/common-headers-interceptor.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { IndexComponent } from './index/index.component';
+import { ArtistComponent } from './artist/artist.component';
+import { EventsComponent } from './events/events.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookComponent } from './book/book.component';
+import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { OptionsComponent } from './options/options.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignInComponent,
+    IndexComponent,
+    ArtistComponent,
+    EventsComponent,
+    SettingsComponent,
+    ContactComponent,
+    BookComponent,
+    ErrorComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [
     {provide: APP_CONFIG, useValue: environment},
