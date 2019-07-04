22eb16506fedd6dbdbc3eb1e39af8b31d030406d
clesauln <clesauln@protonmail.com>
2019-07-04 17:22:38 +0200

    2_5_ formulaire mise en forme

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  idUserCtrl:FormControl;
+  passwordCtrl:FormControl;
+  passwordConfirmCtrl:FormControl;
+  emailCtrl:FormControl;
+  citiesCtrl: FormControl;
+  formInscription:FormGroup;
+
+  cities: string[] = [
+    'Lyon Rhône',
+    'Marseille Bouches Rhône',
+    'Rennes, Ille-et-Vilaine'
+  ]
+
+  isArtist:boolean = false;
+
+  city: string = this.cities[0];
+  
+  constructor(private fb:FormBuilder) {
+
+      this.idUserCtrl = this.fb.control("",[Validators.required]);
+      this.passwordCtrl = this.fb.control("",[Validators.required]);
+      this.passwordConfirmCtrl = this.fb.control("",[Validators.required]);
+      this.emailCtrl = this.fb.control("",[Validators.required, Validators.email]);
+
+      this.formInscription = this.fb.group(
+        {
+          idUser : this.idUserCtrl,
+          password: this.passwordCtrl,
+          passwordConfirm: this.passwordConfirmCtrl,
+          email: this.emailCtrl,
+          city: this.citiesCtrl
+        }
+      );
+
+   }
 
   ngOnInit() {
   }
 
+  
 }
+
+
+
