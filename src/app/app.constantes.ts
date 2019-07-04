83002c2a125556017250aebdd865082affbd229a
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-07-04 11:36:15 +0200

    1_Routage (#1)
    Created Header, SideMenu & Options components as well

---
+++ src/app/app.constantes.ts
@@ -0,0 +1,13 @@
+//Routes
+export const PATH_INDEX = '';
+export const PATH_HOME = 'home';
+export const PATH_LOGIN = 'login';
+export const PATH_SIGN_IN = 'signin';
+export const PATH_ARTIST = PATH_HOME + '/artist';
+export const PATH_BOOK = PATH_HOME + '/book';
+export const PATH_CONTACT = 'contact';
+export const PATH_EVENTS = PATH_HOME + '/events';
+export const PATH_SETTINGS = 'settings';
+export const PATH_ERROR = '404';
+
+
