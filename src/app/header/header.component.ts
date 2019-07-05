18445da1a4a4cb7b5f472b768fc5b0cc1e998bc4
Alexandre MEUR <alex.meur@gmail.com>
2019-07-05 12:18:30 +0200

    3_Menu style done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,16 @@ import {Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {Subscription} from 'rxjs';
 import {PATH_ARTIST, PATH_BOOK, PATH_CONTACT, PATH_EVENTS, PATH_HOME, PATH_INDEX} from '../app.constantes';
-import {MatMenuTrigger} from "@angular/material";
+import {MatMenuTrigger} from '@angular/material';
 
 const TITLE_DEFAULT = 'Private ShowCase';
 const TITLE_HOME = 'Artistes dans votre département';
 const TITLE_ARTIST = 'Fiche Artiste';
 const TITLE_BOOK = 'Créez un évènement';
 const TITLE_EVENTS = 'Évènements';
+
+const MENU_ICON_CLOSED = 'menu';
+const MENU_ICON_OPENED = 'close';
 
 @Component({
 	selector: 'app-header',
@@ -18,19 +21,15 @@ const TITLE_EVENTS = 'Évènements';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
-	@ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-
 	title = '';
+	subscribeCurrentUrl: Subscription;
 	displayBackButton = false;
 	displaySideMenu = false;
+	openSideMenu = false;
 	displayOptions = false;
-	subscribeCurrentUrl: Subscription;
+	sideMenuIcon = MENU_ICON_CLOSED;
 
 	constructor(private location: Location, private router: Router) {
-	}
-
-	closeMenu() {
-		this.trigger.closeMenu();
 	}
 
 	/**
@@ -58,7 +57,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	 * Indicates if the header displays the back button or not
 	 */
 	isBackButtonDisplayed(path: string){
-		console.log('isBackButtonDisplayed PATH', path);
 		let result: boolean;
 		if (path === '/' + PATH_HOME || path === PATH_INDEX) {
 			result = false;
@@ -81,6 +79,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
 		return result;
 	}
 
+	/**
+	 * Handle clicks on the menu to open/close it and change its icon
+	 */
+	handleMenuButtonClick() {
+		this.sideMenuIcon = this.sideMenuIcon === MENU_ICON_CLOSED ? MENU_ICON_OPENED : MENU_ICON_CLOSED;
+		this.openSideMenu = !this.openSideMenu;
+	}
+
 	navigateToIndex() {
 		this.router.navigate([PATH_INDEX]);
 	}
@@ -98,6 +104,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnInit() {
+		// Subscribing to url changes to adapt the header
 		this.subscribeCurrentUrl = this.router.events.subscribe(() => {
 			this.title = this.getTitleFrom(this.location.path());
 			this.displayBackButton = this.isBackButtonDisplayed(this.location.path());
