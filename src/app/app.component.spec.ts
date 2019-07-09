4eb4e5b02d79ba08147182fbf44905097e5655ae
Clement Lesaulnier <clemansles@gmail.com>
2019-07-09 11:45:22 +0200

    4 us002 signin (#14)
    * 2_5_ formulaire mise en forme
    
    * 2_us002_form signin inputs wrapper center
    
    * 2_us002_form inscription mise en forme
    
    * 3_US002_form signin json user
    
    * pull develop
    
    * 3_US002_form signin lint
    
    * 3_US002_form signin tslint
    
    * 3_US002_form signin test
    
    * 4_US002_signin confirm password
    
    * 4_US002_signin password check
    
    * 4_US002_signin inscription artiste user
    
    * 4_US002_signin password control
    
    * test app component
    
    * 4_US002_navigation signin
    
    * 4_US002_signin merge orgin

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -38,9 +38,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to private-showcase!');
   }));
 
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    }));
 });
