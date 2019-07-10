365556886ff7ed23caa83d46e8de2582dba494b5
clesauln <clesauln@protonmail.com>
2019-07-10 15:07:56 +0200

    lint validator service

--- src/app/validators/validatorService.ts
+++ src/app/validators/validatorService.ts
@@ -11,7 +11,7 @@ export class ValidatorService {
   checkLoginNotTaken(login: string) {
       return this.http.get('http://localhost:8080/common/').pipe(
       delay(1000),
-      map(res => {console.log('result', res); return res;}),
+      map(res => {console.log('result', res); return res; }),
       map((users: any[]) => users.filter(user => user.username === login)),
       map(users => !users.length),
           catchError((er, caught) => {console.log('error : ', er); return of(true); })
