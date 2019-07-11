4a618051c885d4ad1fd0577a46cd8a8ccbd432a4
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-07-11 15:21:58 +0200

    22_US003 v1

--- src/app/services/artist/artist-repository.ts
+++ src/app/services/artist/artist-repository.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {EnvironmentService} from '../environment.service';
+import {Artist} from '../../user/artist';
+import {Observable} from 'rxjs';
 
 const RESOURCES_ARTIST_BY_USERNAME = '/artist/name/';
+const RESOURCES_ARTIST_ADD = '/artist/';
+const RESOURCSE_WITH_PASSWORD = '?password=';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +18,14 @@ export class ArtistRepository {
     getArtistByUsername(username: string) {
         return this.http.get(`${this.env.getPrivateShowcaseApiConfig().uri}${RESOURCES_ARTIST_BY_USERNAME}${username}`);
     }
+
+	/**
+	* Add new Artist
+	*/
+	addArtist(artist: Artist, password: string): Observable<any> {
+		return this.http.put(
+			`${this.env.getPrivateShowcaseApiConfig().uri}${RESOURCES_ARTIST_ADD}${RESOURCSE_WITH_PASSWORD}${password}`,
+			JSON.stringify(artist)
+		);
+	}
 }
