6034f2b2137bf334e810b3d5230fe5082e58ab59
Alexandre MEUR <alex.meur@gmail.com>
2019-07-12 17:22:43 +0200

    Started form in artist-panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MatToolbarModule, MatIconModule, MatNativeDateModule, MatInputModule} from '@angular/material';
+import {MatToolbarModule, MatIconModule, MatNativeDateModule, MatInputModule, MatFormFieldModule} from '@angular/material';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
-
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
--- src/app/artist-panel/artist-panel.component.ts
+++ src/app/artist-panel/artist-panel.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'app-artist-panel',
@@ -13,7 +14,28 @@ export class ArtistPanelComponent implements OnInit {
     @Input()
     displayEdition = false;
 
-    constructor() {
+    inputNameDisabled = true;
+    inputShortDescriptionDisabled = true;
+    inputLongDescriptionDisabled = true;
+
+    nameCtrl: FormControl;
+    shortDescCtrl: FormControl;
+    longDescCtrl: FormControl;
+    artistForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.nameCtrl = fb.control('');
+        this.shortDescCtrl = fb.control('');
+        this.longDescCtrl = fb.control('');
+        this.artistForm = fb.group({
+            name: this.nameCtrl,
+            shortdesc: this.shortDescCtrl,
+            longDesc: this.longDescCtrl,
+        });
+    }
+
+    handleSubmit() {
+        console.log('TODO');
     }
 
     ngOnInit() {
